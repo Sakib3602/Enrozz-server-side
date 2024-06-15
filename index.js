823b393ee04dceacfb5cb39c74f4315e404ecb0f
Sakib3602 <sakibsarker6969@gmail.com>
2024-06-15 12:20:23 +0600

    first get done

--- index.js
+++ index.js
@@ -24,11 +24,55 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const sliderDB = client.db("Enrozzz").collection("sliderData");
+
+    // slider all data get 
+    app.get("/slider", async (req, res) => {
+        const sliderData = await sliderDB.find().toArray();
+        res.send(sliderData);
+    })
+    // slider all data get end
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
