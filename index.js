d286a6ae7535ea5f08a946a13c5229bfb3b9cd1e
Sakib3602 <sakibsarker6969@gmail.com>
2024-06-16 12:13:06 +0600

    user data post done

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const cors = require('cors');
-app.use(cors())
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json())
+require("dotenv").config();
 
-// 
+//
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,22 +27,30 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const sliderDB = client.db("Enrozzz").collection("sliderData");
+    const userDB = client.db("Enrozzz").collection("userData");
 
-    // slider all data get 
+    // slider all data get
     app.get("/slider", async (req, res) => {
-        const sliderData = await sliderDB.find().toArray();
-        res.send(sliderData);
-    })
+      const sliderData = await sliderDB.find().toArray();
+      res.send(sliderData);
+    });
     // slider all data get end
-    // slider single data get 
+    // slider single data get
     app.get("/slider/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = {_id : new ObjectId(id)};
-        const sliderData = await sliderDB.findOne(query)
-        res.send(sliderData);
-    })
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const sliderData = await sliderDB.findOne(query);
+      res.send(sliderData);
+    });
     // slider single data get end
-    
+
+    // user Data save to db
+    app.post("/userData", async (req, res) => {
+      const doc = req.body;
+      console.log(doc)
+      const result = await userDB.insertOne(doc);
+      res.send(result);
+    });; 
 
 
 
@@ -54,30 +63,10 @@ async function run() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -85,23 +74,12 @@ async function run() {
 }
 run().catch(console.dir);
 
+//
 
-
-
-
-
-
-
-
-
-
-
-// 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
